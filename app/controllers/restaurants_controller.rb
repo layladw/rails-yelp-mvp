f75ca96cecc24d11e85f13d530e10e758cb3fdc0
Layla de Waegh <layladewaegh@hotmail.com>
2021-11-10 15:36:49 +0100

    rails

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -8,19 +8,26 @@ class RestaurantsController < ApplicationController
   end
 
   def new
-    # @restaurant = Restaurant.new
+    @restaurant = Restaurant.new
   end
 
   def create
-    # @restaurant = Restaurant.new(restaurant_params)
-    # @restaurant.save
-    # redirect_to task_path(@task)
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+    redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
 
   private
 
   def find_restaurant
     Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
 
 end
